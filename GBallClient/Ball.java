aa35d78c8930358627fe4e087765e359ee5cec07
Johansson <b15sebjo@hs.local>
2017-02-23 15:41:44 +0100

    Added a fucking defautl thinghnnnny fuck you +
    yellow warning.

--- GBallClient/Ball.java
+++ GBallClient/Ball.java
@@ -5,6 +5,10 @@ import java.awt.Graphics;
 import Shared.*;
 
 public class Ball extends GameEntity {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Color m_color;
 
 	public Ball(final Vector2D position, final Vector2D speed) {
